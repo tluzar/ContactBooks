e8fec8e3b1d22e8c44d2ed2f671398cf5099f0d0
tluzar <olitur@poczta.fm>
2017-03-26 13:31:14 +0200

    Update ContactService.java

--- ContactBooks/ContactBooks/src/main/java/com/tluzar/service/ContactService.java
+++ ContactBooks/ContactBooks/src/main/java/com/tluzar/service/ContactService.java
@@ -4,10 +4,10 @@ import java.util.List;
 import com.tluzar.entity.Contact;
 
 public interface ContactService {
-	public long createContact(Contact contact);
+    public long createContact(Contact contact);
     public Contact updateContact(Contact contact);
     public void deleteContact(long id);
     public List<Contact> getAllContacts();
     public Contact getContact(long id);	
-	public List<Contact> getAllContacts(String contactName);
+    public List<Contact> getAllContacts(String contactName);
 }
